9254897e5d29227f56f117f9b58384d08429251a
Guilherme de Souza <dsouzaleite49.gmail.com>
2025-05-29 14:59:35 -0300

    logica_de_programacao

---
+++ Desafio_Quebra_Cabeça.js
@@ -0,0 +1,105 @@
+
+
+
+
+
+
+
+ Montar o Quebra-Cabeça JS 
+
+Objetivo: Remontar um código embaralhado, identificar partes falsas e explicar o raciocínio 
+
+INSTRUÇÕES: 
+
+Você receberá linhas de código embaralhadas. 
+
+Seu desafio é: 
+
+Identificar e separar as linhas corretas. 
+
+Organizar as linhas na ordem certa para o código funcionar. 
+
+
+
+
+
+
+
+Comentar cada bloco dizendo o que ele faz. 
+
+Executar o código e tirar print da saída (ou escrever a saída esperada). 
+
+ 
+
+ 
+
+ 
+
+
+
+Código 1 
+
+ 
+
+
+Link do código: https://www.programiz.com/online-compiler/4tHOApgavMlaO 
+
+ 
+
+ 
+
+
+ 
+
+Código 2 
+
+ 
+
+
+Link do código: https://www.programiz.com/online-compiler/2ZzvVHkjtc39X 
+
+ 
+
+ 
+
+ 
+
+
+Código 3 
+
+
+
+
+Link do código: https://www.programiz.com/online-compiler/0lOGMahnvXgoS 
+
+ 
+
+ 
+
+ 
+
+Código 4 
+
+
+
+
+Link do código: https://www.programiz.com/online-compiler/3Umj5LR4AZ8AR 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+Código 5 
+
+
+
+
+Link do código: https://www.programiz.com/online-compiler/7DMXhuGy10dsY 
+
+ 
