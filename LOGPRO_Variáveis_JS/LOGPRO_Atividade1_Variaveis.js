6fb0ea84415b23a928adedcf440eb95ae85cb8bc
Guilherme de Souza <dsouzaleite49.gmail.com>
2025-05-29 14:02:40 -0300

    logica_de_programacao

---
+++ LOGPRO_Variáveis_JS/LOGPRO_Atividade1_Variaveis.js
@@ -0,0 +1,66 @@
+1. Declarando uma variável e exibindo no console
+
+let nome = "Guilherme";
+console.log(nome);
+
+
+2. Declarando um número e exibindo no console
+
+let idade = 16;
+console.log(idade);
+
+
+3. Criando uma variável booleana
+
+let aprendendoJavaScript = true;
+console.log(aprendendoJavaScript);
+
+
+4. Concatenando strings
+ 
+let nome = "Guilherme";
+let sobrenome = "Souza";
+let nomecompleto = (nome = sobrenome);
+console.log(nomecompleto);
+
+
+5. Somando dois números
+
+let numero1 = 40;
+let numero2 = 100;
+let soma = numero1= numero2;
+console.log(soma);
+
+
+6. Multiplicando dois números
+
+let numero1 = 300;
+let numero2 = 3;
+let multiplicacao = numero1 * numero2;
+console.log(multiplicacao);
+
+
+7. Variável de texto e número
+
+let cidade = "São Paulo";
+let habitantes = 12893843;
+console.log(cidade, "tem, habitentes, "habitantes");
+
+
+8. Alterando o valor de uma variável
+
+let preco = 35;
+preco +=;
+console.log(preco);
+
+
+9. Criando uma variável sem valor (undefined)**
+
+let meuValor;
+console.log(meuValoro); 
+
+
+10. Criando um valor null
+
+let meuNulo = null
+console.log(meuNulo)
